0ff6d43ad2fdbf7f088704ad3a81084247229974
BenjaminZak <benjaminzak21@gmail.com>
2020-10-06 18:44:32 +0200

    modif css

--- boutique.php
+++ boutique.php
@@ -3,14 +3,10 @@
 <?php require('inc_header.php') ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel="stylesheet" href="text/css" href="style.css" />
-</head>
 <body>
-	<div>
+	<div class="catalogue">
 		<?php 
 			$result = $mysqli->query('SELECT * FROM catalogue');
-
 			while ($row = $result->fetch_array()) { ?>
 				<form action="get">
 					<p><?php echo $row['produit_nom']?><br>
@@ -18,4 +14,6 @@
 					Prix : <?php echo $row['produit_prix'];?>€<br>
 					<a href="panier.php?id=<?php echo $row['produit_id'] ?>&prix=<?php echo $row['produit_prix'] ?>">Ajouter au panier</a></p>
 				</form>
-				<?php } ?>
+		<?php } ?>
+
+	</div>
